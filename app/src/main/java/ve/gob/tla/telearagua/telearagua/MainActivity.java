7434626fb08c2df0627ce5f0031a32db3032f5a0
miguelamgelchgz <miguelangelchgz@gmail.com>
2018-06-20 15:53:27 -0400

    Dynamic grid

--- app/src/main/java/ve/gob/tla/telearagua/telearagua/MainActivity.java
+++ app/src/main/java/ve/gob/tla/telearagua/telearagua/MainActivity.java
@@ -1,13 +1,15 @@
 package ve.gob.tla.telearagua.telearagua;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
 
     @Override
@@ -15,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
+    @Override
+    public void onRefresh() {
+        System.out.println("Refresh");
+    }
 }
