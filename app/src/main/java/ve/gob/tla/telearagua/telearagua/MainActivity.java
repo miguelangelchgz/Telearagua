3b0b26631ba89e3286bf0be307550a74ae37e6fc
miguelamgelchgz <miguelangelchgz@gmail.com>
2018-06-26 15:49:03 -0400

    Refresh content pt1

--- app/src/main/java/ve/gob/tla/telearagua/telearagua/MainActivity.java
+++ app/src/main/java/ve/gob/tla/telearagua/telearagua/MainActivity.java
@@ -3,21 +3,29 @@ package ve.gob.tla.telearagua.telearagua;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.GridView;
-import android.widget.Toast;
+import android.text.TextUtils;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+    private RequestQueue mRequestQueue;
 
+
+    public static final String TAG = MainActivity.class
+            .getSimpleName();
+
+
+    private static MainActivity mInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
+
+        /*GridView gridview = (GridView) findViewById(R.id.gridview);
 
         gridview.setAdapter(new ImageAdapter(this));
 
@@ -27,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 Toast.makeText(MainActivity.this, "" + position,
                         Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
 
     }
@@ -36,4 +44,32 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     public void onRefresh() {
         System.out.println("Refresh");
     }
+    public static synchronized MainActivity getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+
 }
--- app/src/main/java/ve/gob/tla/telearagua/telearagua/Post.java
+++ app/src/main/java/ve/gob/tla/telearagua/telearagua/Post.java
@@ -1,14 +1,16 @@
 package ve.gob.tla.telearagua.telearagua;
 
 public class Post {
-    public Post(String title, String link, String image_link, String description) {
+    public Post(String title, String link, String image_link, String description, String date, String category) {
         this.title = title;
         this.link = link;
         this.image_link = image_link;
         this.description = description;
+        this.date = date;
+        this.category = category;
 
     }
 
-    public String description, title, link, image_link;
+    public String description, title, link, image_link,date,category;
 
 }
